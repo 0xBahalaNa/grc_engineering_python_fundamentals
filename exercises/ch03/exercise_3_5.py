cb878258a520dc59bd7e7431dfa542e579630343
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-14 15:37:23 -0800

    feat(ch03): complete exercise 3-5 framework list

---
+++ exercises/ch03/exercise_3_5.py
@@ -0,0 +1,12 @@
+frameworks = ["FedRAMP", "SOC 2", "NIST 800-53 Rev 5", "CJIS", "ISO 27001:2022"]
+
+print(sorted(frameworks))
+print(frameworks)
+
+frameworks.sort()
+print(frameworks)
+
+frameworks.reverse()
+print(frameworks)
+
+print(len(frameworks))
