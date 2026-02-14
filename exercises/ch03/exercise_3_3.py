ed362b6cdf54d9503545a81d0af7ba90e0d6be8e
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-14 15:16:21 -0800

    feat(ch03): complete exercise 3-3 adding controls to scope

---
+++ exercises/ch03/exercise_3_3.py
@@ -0,0 +1,14 @@
+audit_scope = []
+
+audit_scope.append("AC-2")
+print(audit_scope)
+audit_scope.append("IA-2")
+print(audit_scope)
+audit_scope.append("AU-2")
+print(audit_scope)
+audit_scope.append("SC-28")
+print(audit_scope)
+
+audit_scope.insert(0, "AC-1")
+
+print(audit_scope)
