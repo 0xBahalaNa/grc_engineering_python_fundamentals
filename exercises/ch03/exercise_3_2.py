abcbbc78abb18e5207c53c7c92eebbe0335cb343
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-14 14:06:44 -0800

    feat(ch03): complete exercise 3-2 finding severities

---
+++ exercises/ch03/exercise_3_2.py
@@ -0,0 +1,11 @@
+severities = ["Critical", "High", "Medium", "Low", "Informational"]
+
+print(severities[0])
+print(severities[1])
+print(severities[2])
+print(severities[3])
+print(severities[4])
+
+severities[4] = "Info"
+
+print(severities)
