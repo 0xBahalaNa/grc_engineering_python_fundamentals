6a7c388b6a62bd56356dfd4ec4e0abb8bf61941d
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-15 12:08:49 -0800

    feat(ch04): complete exercise 4-4 severity scores

---
+++ exercises/ch04/exercise_4_4.py
@@ -0,0 +1,11 @@
+severity_score = [4, 3, 3, 2, 4, 1, 2, 3, 4, 2]
+
+print(min(severity_score))
+print(max(severity_score))
+print(sum(severity_score))
+
+average_severity = sum(severity_score) / len(severity_score)
+
+print(average_severity)
+
+print(len(severity_score))
